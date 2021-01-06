8e628985e97a87e25bdafa6e3f8da6d0a30622b2
PRODUÇÃO CTE <pcte@pitai.sp.gov>
2021-01-06 11:46:17 -0300

    Definindo o modelo Entry.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,13 @@ class Topic(models.Model):
 
 		
 
+class Entry(models.Model):
+	topic = models.ForeignKey(Topic)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = 'entries'
+
+	def __str__(self):
+		return self.text[:50] + "..."
