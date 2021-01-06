832c99e9b12e74b2473016ac92c3c3c603c3509d
PRODUÇÃO CTE <pcte@pitai.sp.gov>
2021-01-06 12:11:32 -0300

    Migrando o model Entry.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -10,7 +10,7 @@ class Topic(models.Model):
 		
 
 class Entry(models.Model):
-	topic = models.ForeignKey(Topic)
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
 	text = models.TextField()
 	date_added = models.DateTimeField(auto_now_add=True)
 
