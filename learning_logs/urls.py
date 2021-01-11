d4ed946a62012f213d2f0b3718d5777c68747500
PRODUÇÃO CTE <pcte@pitai.sp.gov>
2021-01-11 16:03:32 -0300

    comecando cap 19 mas nao esta funcionando direito.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'learning_logs'
 urlpatterns = [
 	path('', views.index, name='index'),
 
 	path('topics/', views.topics, name='topics'),
 
 	path('topics/<topic_id>/', views.topic, name='topic'),
+
+	# Página para adicionar um novo assunto 
+	path('new_topic/', views.new_topic, name='new_topic'),
 
 	]
 
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Topic
+from .forms import TopicForm
+
 
 def index(request):
 	return render(request, 'learning_logs/index.html')
 
 
 def topics(request):
-	topics = Topic.objects.order_by('date_added')
-	context = {'topics': topics}
-	return render(request, 'learning_logs/topics.html', context)
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
 
 
 def topic(request, topic_id):
@@ -16,4 +21,25 @@ def topic(request, topic_id):
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+	"""adiciona um novo assunto."""
+	if request.method != 'POST':
+		# Nenhum dado submetido; cria um formulário em branco 
+		form = TopicForm()
+	else:
+		# Dados do POST submetidos; processa os Dados
+		form = TopicForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('learning_logs:topics')	
+
+	# formulário em branco ou inválido 
+	context = {'form': form}
+	return render(request, 'learning_logs/new_topic.html', context)
+
+
+
+
 	
