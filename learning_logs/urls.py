b7618686b20284a492396d738139bf3bb8b9c7c6
PRODUÇÃO CTE <pcte@pitai.sp.gov>
2021-01-12 12:55:35 -0300

    dando erro circular nas entradas

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from . import views
 
 app_name = 'learning_logs'
+
 urlpatterns = [
 	path('', views.index, name='index'),
-
 	path('topics/', views.topics, name='topics'),
-
 	path('topics/<topic_id>/', views.topic, name='topic'),
-
 	# Página para adicionar um novo assunto 
 	path('new_topic/', views.new_topic, name='new_topic'),
+	# Página para adicionar uma nova entrada 
+	path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 
 	]
 
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,14 @@
+# from django.shortcuts import render, redirect
+# # from django.http import HttpResponseRedirect
+# # from django.urls import reverse
+# from .models import Topic, Entry
+# from .forms import TopicForm, EntryForm
+
+
 from django.shortcuts import render, redirect
-# from django.http import HttpResponseRedirect
-from django.urls import reverse
-from .models import Topic
-from .forms import TopicForm
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -38,6 +44,30 @@ def new_topic(request):
 	# formulário em branco ou inválido 
 	context = {'form': form}
 	return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+	"""Acrescenta uma nova entrada para um assunto em particular."""
+	topic = Topic.objects.get(id=topic_id)
+
+	if request.method != 'POST':
+		# Nenhum dado submetido; cria um formulário em branco 
+		form = EntryForm()
+	else:
+		# Dados de POST submetidos; processa os Dados
+		form = EntryForm(data=request.POST)
+		if form.is_valid:
+			new_entry = form.save(commit=False)
+			new_entry.topic = topic 
+			new_entry.save()
+			return redirect('learning_logs:topic', topic_id=topic_id)
+
+	context = {'topic': topic, 'form': form}
+	return render(request, 'learning_logs/new_entry.html', context)
+
+
+
+
 
 
 
