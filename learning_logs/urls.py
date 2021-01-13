3742b82888f477f45dcdd68ec58fefeccb882337
PRODUÇÃO CTE <pcte@pitai.sp.gov>
2021-01-13 12:37:48 -0300

    Pronto para CRIANDO CONTAS DE USUARIO.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
 	path('new_topic/', views.new_topic, name='new_topic'),
 	# Página para adicionar uma nova entrada 
 	path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+	# Para editar as entradas já existentes 
+	path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 
 	]
 
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -15,7 +15,6 @@ def index(request):
 	return render(request, 'learning_logs/index.html')
 	
 
-
 def topics(request):
     """Show all topics."""
     topics = Topic.objects.order_by('date_added')
@@ -66,6 +65,26 @@ def new_entry(request, topic_id):
 
 	context = {'topic': topic, 'form': form}
 	return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
 
 
 
