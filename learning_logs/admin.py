e9005ee4b74af995d877de13d8df65cb721051a9
Eliel Coutinho <elielkoutinho@gmail.com>
2021-01-17 03:18:04 -0300

    Funcionando a busca no site admin do django. Falta por na pagina principal.

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from learning_logs.models import Topic, Entry
+from .models import Topic, Entry
 
-admin.site.register(Topic)
-admin.site.register(Entry)
+
+class TopicAdmin(admin.ModelAdmin):
+	list_display = ('id', 'text', 'date_added', 'owner')
+	list_display_links = ('id', 'text', 'date_added', 'owner')
+	list_per_page = 10
+	search_fields = ('text',)
+
+
+
+
+class EntryAdmin(admin.ModelAdmin):
+	list_display = ('id', 'topic','text', 'date_added')
+	list_display_links = ('id', 'topic','text', 'date_added')
+	list_per_page = 10
+	search_fields = ('text',)
+
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Entry, EntryAdmin)
 
 
