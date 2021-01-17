c2bb70708026b253427d51dd9c0890f241fa1c28
Eliel Coutinho <elielkoutinho@gmail.com>
2021-01-17 19:15:08 -0300

    com topicos e entradas no site admin.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,13 +5,15 @@ from django.contrib.auth.models import User
 
 
 class Topic(models.Model):
-	text = models.CharField(max_length=200)
-	date_added = models.DateTimeField(auto_now_add=True)
-	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	text = models.CharField(max_length=200, verbose_name='assunto')
+	date_added = models.DateTimeField(auto_now_add=True, verbose_name='Data')
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='autor')
+
+	class Meta:
+		verbose_name_plural = 'tópicos'
 
 	def __str__(self):
 		return self.text
-
 		
 
 class Entry(models.Model):
@@ -20,7 +22,7 @@ class Entry(models.Model):
 	date_added = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
-		verbose_name_plural = 'entries'
+		verbose_name_plural = 'entradas'
 
 	def __str__(self):
 		return self.text[:50] + "..."
